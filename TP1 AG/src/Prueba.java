c0e81a18c26e51cef892ac4826690e92efffc015
Facundo Alvarez <alvfacu@gmail.com>
2015-04-28 16:20:15 -0300

    Agrego evaluacion en funcion y calculo de Fitness

--- TP1 AG/src/Prueba.java
+++ TP1 AG/src/Prueba.java
@@ -4,12 +4,17 @@ import java.util.Random;
 public class Prueba {
 	
 	static String[][] cromosomas = new String[10][30];
-	
-
+	static int[] cromosomasDecimal = new int[10];
+	static float[] valoresEnFuncion = new float[10];
+	static float pc = (float) 0.75;
+	static float pm = (float) 0.05;
+		
 	public static void main(String[] args)
 	{
 		Random rnd = new Random();
 		ArrayList<String> cro = new ArrayList<String>();
+		float sumatoria = 0;
+				
 		for(int i=0;i<10;i++)
 		{
 			String c = "";
@@ -19,7 +24,12 @@ public class Prueba {
 				c = c+cromosomas[i][j];
 			}
 			cro.add(c);
+			cromosomasDecimal[i] = dameDecimal(c);
+			valoresEnFuncion[i] = funcion(dameDecimal(cro.get(i).toString()));
+			sumatoria += valoresEnFuncion[i];
 		}
+		
+		System.out.println("Sumatoria: "+sumatoria);
 		
 		for(int i=0;i<10;i++)
 		{
@@ -27,8 +37,7 @@ public class Prueba {
 			{
 				System.out.print(cromosomas[i][j]);
 			}
-			System.out.print(" "+dameDecimal(cro.get(i).toString()));
-			System.out.println(" "+funcion(dameDecimal(cro.get(i).toString())));
+			System.out.println(" "+cromosomasDecimal[i]+" "+valoresEnFuncion[i]+" "+valoresEnFuncion[i]/sumatoria/*fitness*/);
 		}
 			
 	}
@@ -41,12 +50,12 @@ public class Prueba {
 		return num;
 	}
 	
-	private static double funcion(int nro)
+	private static float funcion(int nro)
 	{
 		int coef = (int)Math.pow(2,30) - 1;
-		double rdo = 0;
-		double div = (double)nro/(double)coef;
-		rdo = (double)Math.pow(div, 2);		
+		float rdo = 0;
+		float div = (float)nro/(float)coef;
+		rdo = (float)Math.pow(div, 2);		
 		return rdo;
 		
 	}
